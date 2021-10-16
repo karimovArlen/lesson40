78e5a9bcbc2452875c23361588e34a3254690569
karimovArlen <arlen.mm23@gmail.com>
2021-10-16 19:33:38 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        ArrayList<String> listA = new  ArrayList<>();
+        Scanner scannerA = new Scanner(System.in);
+        listA.add(scannerA.nextLine());
+        listA.add(scannerA.nextLine());
+        listA.add(scannerA.nextLine());
+        listA.add(scannerA.nextLine());
+        listA.add(scannerA.nextLine());
+        System.out.println(listA);
+
+
+        System.out.println("________");
+
+        ArrayList<String> listB = new ArrayList<>();
+        System.out.println("Введите список B: ");
+        listB.add(scannerA.nextLine());
+        listB.add(scannerA.nextLine());
+        listB.add(scannerA.nextLine());
+        listB.add(scannerA.nextLine());
+        listB.add(scannerA.nextLine());
+        System.out.println(listB);
+
+        System.out.println("________");
+
+        ArrayList<String> listC = new ArrayList<>();
+        listC.add(listA.get(1));
+        listC.add(listB.get(5));
+        listC.add(listA.get(2));
+        listC.add(listB.get(4));
+        listC.add(listA.get(3));
+        listC.add(listB.get(3));
+        listC.add(listA.get(4));
+        listC.add(listB.get(2));
+        listC.add(listA.get(5));
+        listC.add(listB.get(1));
+        System.out.println(listC);
+
+        listC.sort(Comparator.comparing(String::length));
+        Collections.reverse(listC);
+        System.out.println(listC);
+    }
+}
